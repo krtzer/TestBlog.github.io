1151e3858e62070824e327d0a6baccbd0ae9e4bd
krtzer <kurt.petrisen@gmail.com>
2019-03-28 07:53:11 -0500

    adding new files

--- content/projects/box-counting-sound/Read and Print Sample Rate.py
+++ content/projects/box-counting-sound/Read and Print Sample Rate.py
@@ -4,6 +4,6 @@ import soundfile as sf
 #problems with git -> pathspec doesn't work with flac
 # References
 # https://stackoverflow.com/questions/45198277/pycharm-asks-me-if-i-want-to-add-idea-vcs-xml-to-git
-#
+# 
 data, samplerate = sf.read(r'C:\Users\krtzer\Documents\BlogWebSite\Python Playground\APPLAUSE.wav')
 print (samplerate)
