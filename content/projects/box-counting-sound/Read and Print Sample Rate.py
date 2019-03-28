d568d240df3514e269e5f510aef89477718743c4
krtzer <kurt.petrisen@gmail.com>
2019-03-28 07:39:25 -0500

    Testing out GIT integration in VS code

--- content/projects/box-counting-sound/Read and Print Sample Rate.py
+++ content/projects/box-counting-sound/Read and Print Sample Rate.py
@@ -2,6 +2,8 @@ import soundfile as sf
 
 #https://stackoverflow.com/questions/36399381/whats-the-fastest-way-of-checking-if-a-point-is-inside-a-polygon-in-python
 #problems with git -> pathspec doesn't work with flac
-
+# References
+# https://stackoverflow.com/questions/45198277/pycharm-asks-me-if-i-want-to-add-idea-vcs-xml-to-git
+#
 data, samplerate = sf.read(r'C:\Users\krtzer\Documents\BlogWebSite\Python Playground\APPLAUSE.wav')
 print (samplerate)
