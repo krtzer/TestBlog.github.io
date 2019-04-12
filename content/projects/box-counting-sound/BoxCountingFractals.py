51075c86328096d7f36985b5b59ab95552d13096
krtzer <kurt.petrisen@gmail.com>
2019-04-12 09:00:53 -0500

    the code works but there is a bug in the box counting algorithm

--- content/projects/box-counting-sound/BoxCountingFractals.py
+++ content/projects/box-counting-sound/BoxCountingFractals.py
@@ -14,23 +14,31 @@ from matplotlib.collections import PatchCollection
 from descartes import PolygonPatch
 from geopandas import GeoSeries
 
-'''def MakeGrid (sideLength, audiodata song):
-    	x1, x2, y1, y2 = 0
-		for (y2 < 1): 
-			for (x2 < len(song), )
-		x1 = 0
-		x2
-		for (length(song))
-		return fractalDimention
-'''
+def CountBoxes (boxes, points):
+
+    countedboxes = 0
+    PointInBox = False
+    x = 0
+    for box in boxes:
+        for point in points:
+            if box.contains(point):
+                PointInBox = True
+                #points.remove(point)
+        if PointInBox:
+            countedboxes +=1
+            boxes.remove(box)
+            PointInBox = False
+        x += 1
+    return countedboxes
+
 data, samplerate = sf.read(r'C:\Users\krtzer\Documents\albino-grackle\content\projects\box-counting-sound\021000000.wav')
 
-#dataInPoints = GeoSeries(map(Point, data[:300,:]))
+FirstXSamples = data[:300,0]
 
 dataInPoints = []
 
-for x, y in data:
-    	dataInPoints.append(Point(x,y))
+for index, point in enumerate(FirstXSamples):
+    	dataInPoints.append(Point(index,point))
 
 # length of squre in samples
 GridUnitLength = 2**21
@@ -38,15 +46,12 @@ GridUnitLength = 2**21
 # need to figure out how to what the bitness if of the signal 
 # seems related to bitrate. This signal is 64 kbps
 
-Time = np.linspace(0, len(data)/samplerate, len(data))
+#last parmater 
 
-xvertices = np.linspace(0, len(data), len(data)/GridUnitLength)/samplerate
+Time = np.linspace(0, len(FirstXSamples)/samplerate, len(FirstXSamples))
+
+xvertices = np.linspace(0, len(FirstXSamples), len(data)/GridUnitLength)
 yvertices = np.linspace(-1, 1, len(data)/GridUnitLength)
-
-#xvertices = [0,1,2,3]
-#yvertices = [-2, -1,0,1,2]
-
-#xx,yy = np.meshgrid(xvertices,yvertices)
 
 hlines = [((x1, yi), (x2, yi)) for x1, x2 in zip(xvertices[:-1], xvertices[1:]) for yi in yvertices]
 vlines = [((xi, y1), (xi, y2)) for y1, y2 in zip(yvertices[:-1], yvertices[1:]) for xi in xvertices]
@@ -55,28 +60,10 @@ intermeditateSum = MultiLineString(hlines + vlines)
 
 grids = list(polygonize(intermeditateSum))
 
-# Make grid ot match 
-#xv,yv = np.meshgrid(np.linspace(0, len(data),100),Time)
-
-
-# Copied from https://stackoverflow.com/questions/36399381/whats-the-fastest-way-of-checking-if-a-point-is-inside-a-polygon-in-python 
-first = -3
-size  = (3-first)/100
-xv,yv = np.meshgrid(np.linspace(-3,3,100),np.linspace(-3,3,100))
-p = path.Path([(0,0), (0, 1), (1, 1), (1, 0)])  # square with legs length 1 and bottom left corner at the origin
-flags = p.contains_points(np.hstack((xv.flatten()[:,np.newaxis],yv.flatten()[:,np.newaxis])))
-grid = np.zeros((101,101),dtype='bool')
-grid[((xv.flatten()-first)/size).astype('int'),((yv.flatten()-first)/size).astype('int')] = flags
-
-xi,yi = np.random.randint(-300,300,100)/100,np.random.randint(-300,300,100)/100
-vflag = grid[((xi-first)/size).astype('int'),((yi-first)/size).astype('int')]
-plt.imshow(grid.T,origin='lower',interpolation='nearest',cmap='binary')
-plt.scatter(((xi-first)/size).astype('int'),((yi-first)/size).astype('int'),c=vflag,cmap='Blues',s=90)
-plt.show()
-# End copied
-
+NumberofGrids = CountBoxes(grids, dataInPoints)
 
 BLUE = '#6699cc'
+SOMECOLOR = '#cc00cc'
 fig = plt.figure(1)
 ax = fig.gca()
 #p = PatchCollection(grids, cmap=cm.jet, alpha=0.4)
@@ -87,8 +74,8 @@ ax = fig.gca()
 for grid in grids:
     	ax.add_patch(PolygonPatch(grid, fc=BLUE, ec=BLUE, alpha=0.5, zorder=2 ))
 
-for point in dataInPoints:
-		ax.add_patch(PolygonPatch(point, fc='#cc00cc', ec='#cc00cc', alpha=0.5, zorder=2))
+#for point in dataInPoints:
+#		ax.add_patch(PolygonPatch(point, fc=SOMECOLOR, ec=SOMECOLOR, alpha=0.5, zorder=2))
 plt.title('Songs Waveform')
-#plt.plot(Time[0:3000000],data[0:3000000,0])
+plt.plot(FirstXSamples, 'bs')
 plt.show()
